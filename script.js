5c0892b4f8bb0c8e7389bc1c75e83057f5525d92
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-05-30 21:35:36 +0530

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let bookslist = document.getElementById("bookslist");
 let authorlist = document.getElementById("authorlist");
 let pagelist = document.getElementById("pagelist");
+let statuslist = document.getElementById("statuslist");
+let removelist = document.getElementById("removelist");
 
 const myLibrary = [];
 
@@ -9,6 +11,8 @@ const addbook = document.getElementById("addbook");
 const removebook = document.getElementById("removebook");
 const close = document.getElementById("cancel-button");
 const submit = document.getElementById("submit-button");
+
+
 
 addbook.addEventListener("click", ()=>
 {
@@ -43,16 +47,116 @@ function Book(bookname, authorname, pagecount, readcheck, uid){
     display();
 }
 
+Book.prototype.togglereadstatus = function(value){
+
+    this.readcheck = value;
+}
+
+
+function radiobuttoneventlistener(radio1, radio2, id, div){
+
+    booktemp = myLibrary.find(book => book.uid === id);
+
+    radio1.addEventListener("click",()=>
+    {
+        booktemp.togglereadstatus("read");
+        div.classList.add("statuscolor-green");
+        div.classList.remove("statuscolor-red");
+    })
+
+    radio2.addEventListener("click",()=>
+    {
+        booktemp.togglereadstatus("notread");
+        div.classList.add("statuscolor-red");
+        div.classList.remove("statuscolor-green");
+    })
+}
+
+function showradiobutton(uid, readcheck){
+
+    const radio1 = document.createElement("input");
+    radio1.type = "radio";
+    radio1.name = `read-status-${uid}`;
+    radio1.id = `read-${uid}`;
+    radio1.classList.add("radiobutton-styling");
+    
+    const label1 = document.createElement("label");
+    label1.setAttribute('for',`read-${uid}`);
+    label1.textContent = "Read";
+
+    const radio2 = document.createElement("input");
+    radio2.type = "radio";
+    radio2.name = `read-status-${uid}`;
+    radio2.id = `notread-${uid}`;
+    radio2.classList.add("radiobutton-styling");
+
+    const label2 = document.createElement("label");
+    label2.setAttribute('for',`notread-${uid}`);
+    label2.textContent = "Not Read";
+
+
+
+    // wrapping for css 
+    const div = document.createElement("div");
+    div.append(radio1, label1, radio2, label2);
+    div.classList.add("radio-flex");
+    statuslist.append(div);
+
+
+
+    if(readcheck == "read")
+    {
+        document.getElementById(`read-${uid}`).checked = true;
+        div.classList.add("statuscolor-green");
+        div.classList.remove("statuscolor-red");
+    }
+    else{
+        document.getElementById(`notread-${uid}`).checked = true;
+        div.classList.remove("statuscolor-green");
+        div.classList.add("statuscolor-red");
+    }
+
+    radiobuttoneventlistener(radio1, radio2, uid, div);
+
+}
+
+
+function deletebook(id){
+
+    const delbutton = document.createElement("input");
+    delbutton.type = "button";
+    delbutton.id = `delete-button-${id}`;
+    delbutton.value = "DELETE BOOK";
+    delbutton.classList.add("button_style");
+
+    const delete_div = document.createElement("div");
+    delete_div.append(delbutton)
+    delete_div.classList.add("delete-flex")
+    removelist.append(delete_div);
+
+    delbuttoneventlistener()
+}
+
 
 function display(){
     bookslist.innerHTML = ""
     authorlist.innerHTML = ""
     pagelist.innerHTML = ""
-    
+    statuslist.innerHTML = ""
+    removelist.innerHTML = ""
     myLibrary.forEach(e => {
+
         const booktag = document.createElement("h2");
         const authortag = document.createElement("h2");
         const pagetag = document.createElement("h2");
+
+        booktag.innerHTML = e.bookname;
+        authortag.innerHTML = e.authorname;
+        pagetag.innerHTML = e.pagecount;
+
+        bookslist.append(booktag);
+        authorlist.append(authortag);
+        pagelist.append(pagetag);
 
         //styling
 
@@ -63,64 +167,8 @@ function display(){
                 element.classList.add("fancytext");
             }
         });
-
-        booktag.innerHTML = e.bookname;
-        authortag.innerHTML = e.authorname;
-        pagetag.innerHTML = e.pagecount;
-
-        bookslist.append(booktag);
-        authorlist.append(authortag);
-        pagelist.append(pagetag);
+        
+        deletebook(e.uid);
+        showradiobutton(e.uid, e.readcheck);
     });
 }
-
-
-//styling
-
-
-// addbutton.addEventListener("click", ()=>
-// {
-//     addBook();
-//     displaybook();
-// });
-
-// let bookscontainer = document.getElementById("books");
-// let authorcontainer = document.getElementById("author");
-// let pagecontainer = document.getElementById("pages");
-
-// function displaybook()
-// {
-//     bookscontainer.innerHTML = "";
-//     authorcontainer.innerHTML = "";
-//     pagecontainer.innerHTML = "";
-
-//     myLibrary.forEach(e => {
-//         const bookdiv = document.createElement("div");
-//         // div.style.color = "blue";
-//         bookdiv.style.fontSize = "35px";
-//         bookdiv.innerHTML = e.bookname;
-//         bookscontainer.append(bookdiv);
-        
-//         const authordiv = document.createElement("div");
-//         authordiv.style.fontSize = "35px";
-//         authordiv.innerHTML = e.author;
-//         authorcontainer.append(authordiv);
-
-//         const pagediv = document.createElement("div");
-//         pagediv.style.fontSize = "35px";
-//         pagediv.innerHTML = e.pages;
-//         pagecontainer.append(pagediv);
-//     });
-// }
-
-// bookslist.innerHTML += bookname;
-// authorlist.innerHTML += authorname;
-// pagelist.innerHTML += pagecount;
-
-// if(readcheck){ // selected radio button:
-//     authorlist.innerHTML = "Hello there";
-// }
-
-// else{
-
-// }
